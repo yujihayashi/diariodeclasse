bdd13c051d5cacf94aab20349215a6df5e1ecfb8
Yuji Hayashi <arthuryuji@hotmail.com>
2015-10-16 15:23:49 -0300

    Apagando elementos não utilizados

--- js/bootstrap_site.js
+++ js/bootstrap_site.js
@@ -1,50 +1,49 @@
 $(function() {
-    $('#load').css({opacity: 0.6});
-    $("#load").ajaxStart(function() {
-        $(this).show();
-    });
-    $("#load").ajaxStop(function() {
-        $(this).hide();
-    });
 
-    
-// $('.ver-imagem').prettyPhoto();
-// $('a[rel^="prettyPhoto"]').prettyPhoto();
-});
-$(function() {
-    $('.jImgFormat').find('img').each(function() {
-        var $float = $(this).css('float');
-        $(this).css('float', 'none');
-        var margin = '';
-        switch ($float) {
-            case 'left':
-                margin = 'margin:2px 10px 2px 0px';
-                break;
-            case 'right':
-                margin = 'margin:2px 0px 2px 10px';
-                break;
-            default:
-                break;
+    $('.navbar').css({display:'none'});
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >= '500') {
+            $('.navbar').css({display:'block'});
+        } else {
+            $('.navbar').css({display:'none'});
         }
-        var url_amp = $(this).attr('src');
-        url_amp = url_amp.replace(/-([0-9]*)\./, '.');
-        var img = $(this).wrap('<a href="' + url_amp + '" rel="shadowbox" style="float:' + $float + ';' + margin + '" class="imgTexto"/>');
-        img.after('<strong style="display:block">' + $(this).attr('alt') + '</strong>');
     });
-});
 
-function showPopup() {
-    $('.popup').fadeIn('slow');
-    var popupMarginLeft = $('.popup-img').width() / 2,
-        popupMarginTop = $('.popup-img').height() / 2;
+    /* LOCALSCROLL START */
+/**
+     * Most jQuery.localScroll's settings, actually belong to jQuery.ScrollTo, check it's demo for an example of each option.
+     * @see http://flesler.demos.com/jquery/scrollTo/
+     * You can use EVERY single setting of jQuery.ScrollTo, in the settings hash you send to jQuery.LocalScroll.
+     */
 
-    // console.log(popupMarginLeft);
-    // console.log($('.popup-img img').attr('height'));
-    $('.popup-content').css({marginLeft: -popupMarginLeft + 'px'/*, marginTop: -popupMarginTop+'px'*/});
-    $('.popup-close, .popup').click(function() {
-        $('.popup').fadeOut();
+    // The default axis is 'y', but in this demo, I want to scroll both
+    // You can modify any default like this
+    $.localScroll.defaults.axis = 'y';
+
+    // Scroll initially if there's a hash (#something) in the url 
+    $.localScroll.hash({
+        target: 'body', // Could be a selector or a jQuery object too.
+        queue:true,
+        offset: -115,
+        duration:1000
     });
-    $('.popup-content').click(function() {
-        return false;
+
+    /**
+     * NOTE: I use $.localScroll instead of $('#navigation').localScroll() so I
+     * also affect the >> and << links. I want every link in the page to scroll.
+     */
+     $.localScroll({
+        target: 'body', // could be a selector or a jQuery object too.
+        queue:true,
+        duration:1000,
+        hash:true,
+        offset: -115,
+        //filter: {'.panel-group'},
+        onBefore:function( e, anchor, $target ){
+            // The 'this' is the settings object, can be modified
+        },
+        onAfter:function( anchor, settings ){
+            // The 'this' contains the scrolled element (#content)
+        }
     });
-}
+ });
